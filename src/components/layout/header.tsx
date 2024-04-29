39aa5eb6d099a9455ae242148428130f3386ec57
Rodrigo Dulanto <rodrigo.dulanto@pucp.edu.pe>
2024-04-29 14:14:31 -0500

    feat(layout): makes layout responsive for smaller devices

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
-    <header className="flex items-center justify-center bg-neutral-800  text-white">
-      <div className="flex w-full max-w-screen-lg items-center justify-start gap-3">
+    <header className="flex items-center justify-center bg-neutral-800 text-white">
+      <div className="flex w-full max-w-screen-lg items-center justify-start gap-3 px-4 md:px-6 xl:px-0">
         <h1 className="text-3xl font-thin">
           PHOTO<span className="font-semibold">SEARCH</span>
         </h1>
