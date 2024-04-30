ae0eba76370cdd9bb768fb5f00c367277c6541e8
Rodrigo Dulanto <rodrigo.dulanto@pucp.edu.pe>
2024-04-29 20:05:46 -0500

    fix: fixes ts errors in mocks file

--- src/components/photo-card/__tests__/mocks.ts
+++ src/components/photo-card/__tests__/mocks.ts
@@ -2,17 +2,6 @@ import { Photo } from '../photo-card';
 
 export const photo: Photo = {
   id: '4hMET7vYTAQ',
-  slug: 'mountain-with-clouds-4hMET7vYTAQ',
-  alternative_slugs: {
-    en: 'mountain-with-clouds-4hMET7vYTAQ',
-    es: 'montana-con-nubes-4hMET7vYTAQ',
-    ja: '雲のある山-4hMET7vYTAQ',
-    fr: 'montagne-avec-des-nuages-4hMET7vYTAQ',
-    it: 'montagna-con-le-nuvole-4hMET7vYTAQ',
-    ko: '구름이-있는-산-4hMET7vYTAQ',
-    de: 'berg-mit-wolken-4hMET7vYTAQ',
-    pt: 'montanha-com-nuvens-4hMET7vYTAQ',
-  },
   created_at: '2018-05-15T14:12:45Z',
   updated_at: '2024-04-28T23:49:50Z',
   promoted_at: '2018-05-16T11:51:06Z',
@@ -22,7 +11,6 @@ export const photo: Photo = {
   blur_hash: 'LkGu%lt7NHWC~qS5jsoJ%1IpRjof',
   description: 'Piece of history',
   alt_description: 'mountain with clouds',
-  breadcrumbs: [],
   urls: {
     raw: 'https://images.unsplash.com/photo-1526392060635-9d6019884377?ixid=M3w1OTU0OTl8MHwxfHNlYXJjaHwxfHxwZXJ1fGVufDB8fHx8MTcxNDQxNjE3M3ww&ixlib=rb-4.0.3',
     full: 'https://images.unsplash.com/photo-1526392060635-9d6019884377?crop=entropy&cs=srgb&fm=jpg&ixid=M3w1OTU0OTl8MHwxfHNlYXJjaHwxfHxwZXJ1fGVufDB8fHx8MTcxNDQxNjE3M3ww&ixlib=rb-4.0.3&q=85',
@@ -32,7 +20,6 @@ export const photo: Photo = {
       'https://images.unsplash.com/photo-1526392060635-9d6019884377?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1OTU0OTl8MHwxfHNlYXJjaHwxfHxwZXJ1fGVufDB8fHx8MTcxNDQxNjE3M3ww&ixlib=rb-4.0.3&q=80&w=400',
     thumb:
       'https://images.unsplash.com/photo-1526392060635-9d6019884377?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1OTU0OTl8MHwxfHNlYXJjaHwxfHxwZXJ1fGVufDB8fHx8MTcxNDQxNjE3M3ww&ixlib=rb-4.0.3&q=80&w=200',
-    small_s3: 'https://s3.us-west-2.amazonaws.com/images.unsplash.com/small/photo-1526392060635-9d6019884377',
   },
   links: {
     self: 'https://api.unsplash.com/photos/mountain-with-clouds-4hMET7vYTAQ',
@@ -43,11 +30,6 @@ export const photo: Photo = {
       'https://api.unsplash.com/photos/4hMET7vYTAQ/download?ixid=M3w1OTU0OTl8MHwxfHNlYXJjaHwxfHxwZXJ1fGVufDB8fHx8MTcxNDQxNjE3M3ww',
   },
   likes: 861,
-  liked_by_user: false,
-  current_user_collections: [],
-  sponsorship: null,
-  topic_submissions: {},
-  asset_type: 'photo',
   user: {
     id: 'fk8mQYRRXZM',
     updated_at: '2024-04-28T01:41:39Z',
@@ -80,17 +62,6 @@ export const photo: Photo = {
     total_collections: 25,
     total_likes: 6089,
     total_photos: 931,
-    total_promoted_photos: 344,
-    total_illustrations: 0,
-    total_promoted_illustrations: 0,
-    accepted_tos: true,
-    for_hire: false,
-    social: {
-      instagram_username: 'will_justen',
-      portfolio_url: 'https://willianjusten.com.br/',
-      twitter_username: 'Willian_justen',
-      paypal_email: null,
-    },
   },
   tags: [
     {
@@ -104,127 +75,24 @@ export const photo: Photo = {
     {
       type: 'landing_page',
       title: 'travel',
-      source: {
-        ancestry: {
-          type: {
-            slug: 'images',
-            pretty_slug: 'Images',
-          },
-          category: {
-            slug: 'travel',
-            pretty_slug: 'Travel',
-          },
-        },
-        title: 'Travel images',
-        subtitle: 'Download free travel images',
-        description:
-          "We come across many of life's most beautiful images while travelling. Monuments, the bright lights of big cities like Las Vegas, magnificent landscapes. Unsplash captures it all, with a free selection of gorgeous travelling photos from every corner of the earth.",
-        meta_title: '100+ Travel Pictures | Download Free Images & Stock Photos on Unsplash',
-        meta_description:
-          'Choose from hundreds of free travel pictures. Download HD travel photos for free on Unsplash.',
-        cover_photo: {
-          id: 'KgCbvOWYuU0',
-          slug: 'aerial-photo-of-person-using-paddleboard-KgCbvOWYuU0',
-          alternative_slugs: {
-            en: 'aerial-photo-of-person-using-paddleboard-KgCbvOWYuU0',
-            es: 'foto-aerea-de-una-persona-usando-paddleboard-KgCbvOWYuU0',
-            ja: 'パドルボードを使用する人物の航空写真-KgCbvOWYuU0',
-            fr: 'photo-aerienne-dune-personne-utilisant-une-planche-a-pagaie-KgCbvOWYuU0',
-            it: 'foto-aerea-di-una-persona-che-usa-il-paddleboard-KgCbvOWYuU0',
-            ko: '패들보드를-사용하는-사람의-항공-사진-KgCbvOWYuU0',
-            de: 'luftbild-einer-person-die-ein-paddleboard-benutzt-KgCbvOWYuU0',
-            pt: 'foto-aerea-da-pessoa-usando-paddleboard-KgCbvOWYuU0',
-          },
-          created_at: '2017-01-19T05:49:32Z',
-          updated_at: '2022-12-01T10:58:22Z',
-          promoted_at: null,
-          width: 3199,
-          height: 2242,
-          color: '#73a6a6',
-          blur_hash: 'LBCu-R_N4nS_%hx]M{S1IUWVr[kC',
-          description: 'My name is Ishan I make photos and videos\r\n\r\nwww.instagram.com/seefromthesky',
-          alt_description: 'aerial photo of person using paddleboard',
-          breadcrumbs: [],
-          urls: {
-            raw: 'https://images.unsplash.com/photo-1484804959297-65e7c19d7c9f?ixlib=rb-4.0.3',
-            full: 'https://images.unsplash.com/photo-1484804959297-65e7c19d7c9f?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb',
-            regular:
-              'https://images.unsplash.com/photo-1484804959297-65e7c19d7c9f?ixlib=rb-4.0.3&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max',
-            small:
-              'https://images.unsplash.com/photo-1484804959297-65e7c19d7c9f?ixlib=rb-4.0.3&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max',
-            thumb:
-              'https://images.unsplash.com/photo-1484804959297-65e7c19d7c9f?ixlib=rb-4.0.3&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max',
-            small_s3: 'https://s3.us-west-2.amazonaws.com/images.unsplash.com/small/photo-1484804959297-65e7c19d7c9f',
-          },
-          links: {
-            self: 'https://api.unsplash.com/photos/aerial-photo-of-person-using-paddleboard-KgCbvOWYuU0',
-            html: 'https://unsplash.com/photos/aerial-photo-of-person-using-paddleboard-KgCbvOWYuU0',
-            download: 'https://unsplash.com/photos/KgCbvOWYuU0/download',
-            download_location: 'https://api.unsplash.com/photos/KgCbvOWYuU0/download',
-          },
-          likes: 1273,
-          liked_by_user: false,
-          current_user_collections: [],
-          sponsorship: null,
-          topic_submissions: {
-            wallpapers: {
-              status: 'approved',
-              approved_on: '2020-04-06T14:20:09Z',
-            },
-            nature: {
-              status: 'approved',
-              approved_on: '2020-04-06T14:20:18Z',
-            },
-          },
-          asset_type: 'photo',
-          premium: false,
-          plus: false,
-          user: {
-            id: 'cYNNst8ZosY',
-            updated_at: '2024-04-17T11:34:03Z',
-            username: 'seefromthesky',
-            name: 'Ishan @seefromthesky',
-            first_name: 'Ishan',
-            last_name: '@seefromthesky',
-            twitter_username: 'SeefromtheSky',
-            portfolio_url: 'http://www.seefromthesky.com',
-            bio: '‎لآ اِلَهَ اِلّا اللّهُ مُحَمَّدٌ رَسُوُل اللّهِ\r\n •••  Photographer from Maldives. Peace and love. 🇲🇻 📧 ishan@seefromthesky.com ',
-            location: 'maldives',
-            links: {
-              self: 'https://api.unsplash.com/users/seefromthesky',
-              html: 'https://unsplash.com/@seefromthesky',
-              photos: 'https://api.unsplash.com/users/seefromthesky/photos',
-              likes: 'https://api.unsplash.com/users/seefromthesky/likes',
-              portfolio: 'https://api.unsplash.com/users/seefromthesky/portfolio',
-              following: 'https://api.unsplash.com/users/seefromthesky/following',
-              followers: 'https://api.unsplash.com/users/seefromthesky/followers',
-            },
-            profile_image: {
-              small:
-                'https://images.unsplash.com/profile-1577096080474-f52fd0afc140image?ixlib=rb-4.0.3&crop=faces&fit=crop&w=32&h=32',
-              medium:
-                'https://images.unsplash.com/profile-1577096080474-f52fd0afc140image?ixlib=rb-4.0.3&crop=faces&fit=crop&w=64&h=64',
-              large:
-                'https://images.unsplash.com/profile-1577096080474-f52fd0afc140image?ixlib=rb-4.0.3&crop=faces&fit=crop&w=128&h=128',
-            },
-            instagram_username: 'seefromthesky',
-            total_collections: 0,
-            total_likes: 101,
-            total_photos: 184,
-            total_promoted_photos: 56,
-            total_illustrations: 0,
-            total_promoted_illustrations: 0,
-            accepted_tos: true,
-            for_hire: true,
-            social: {
-              instagram_username: 'seefromthesky',
-              portfolio_url: 'http://www.seefromthesky.com',
-              twitter_username: 'SeefromtheSky',
-              paypal_email: null,
-            },
-          },
-        },
-      },
     },
   ],
+  exif: {
+    make: null,
+    model: null,
+    exposure_time: null,
+    aperture: null,
+    focal_length: null,
+    iso: null,
+  },
+  location: {
+    city: null,
+    country: null,
+    /** full string representation of the location, including `city` and `country` if they exist. */
+    name: null,
+    position: {
+      latitude: null,
+      longitude: null,
+    },
+  },
 };
